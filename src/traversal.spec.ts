fa307dea7ce6616154f12c703a2e8015d9dbf653
Yukimasa funaoka <yukimasafunaoka@gmail.com>
2020-08-14 06:04:46 +0900

    Add nextElementSibling tests

--- src/traversal.spec.ts
+++ src/traversal.spec.ts
@@ -1,5 +1,6 @@
 import { makeDom } from "./__fixtures__/utils";
 import assert from "assert";
+import { Element } from "domhandler";
 import * as DomUtils from ".";
 
 describe("traversal", () => {
@@ -10,6 +11,24 @@ describe("traversal", () => {
             assert.doesNotThrow(() =>
                 DomUtils.hasAttrib(firstNode as never, "some-attrib")
             );
+        });
+    });
+
+    describe("nextElementSibling", () => {
+        test("return Element if found", () => {
+            const dom = makeDom(
+                "<div><h1></h1>test<p></p></div>"
+            )[0] as Element;
+            const firstNode = dom.children[0];
+
+            const next = DomUtils.nextElementSibling(firstNode) as Element;
+            assert.equal(next.tagName, "p");
+        });
+        test("return null if not found", () => {
+            const dom = makeDom("<div><p></p>test</div>")[0] as Element;
+            const firstNode = dom.children[0];
+
+            assert.equal(DomUtils.nextElementSibling(firstNode), null);
         });
     });
 });
