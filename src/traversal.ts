f64d58c7673a5a7951bd2a1ec11ade9c1ef2eee9
Felix Böhm <me@feedic.com>
2020-09-18 13:59:22 -0700

    fix(traversal): Improve traversal handling of nullish values, docs

--- src/traversal.ts
+++ src/traversal.ts
@@ -1,7 +1,7 @@
 import type { Node, Element } from "domhandler";
 
 export function getChildren(elem: Node | Element): Node[] | null {
-    return (elem as { children?: Node[] }).children || null;
+    return (elem as { children?: Node[] }).children ?? null;
 }
 
 export function getParent(elem: Node): Node | null {
@@ -10,19 +10,30 @@ export function getParent(elem: Node): Node | null {
 
 export function getSiblings(elem: Node): Node[] | null {
     const parent = getParent(elem);
-    return parent ? getChildren(parent) : [elem];
+    return parent != null ? getChildren(parent) : [elem];
 }
 
-export function getAttributeValue(elem: Element, name: string): string {
+/**
+ * Gets an attribute from an element.
+ *
+ * @param elem Element to check.
+ * @param name Attribute name to retrieve.
+ */
+export function getAttributeValue(
+    elem: Element,
+    name: string
+): string | undefined {
     return elem.attribs?.[name];
 }
 
+/**
+ * Checks whether an element has an attribute.
+ *
+ * @param elem Element to check.
+ * @param name Attribute name to look for.
+ */
 export function hasAttrib(elem: Element, name: string): boolean {
-    return (
-        !!elem.attribs &&
-        Object.prototype.hasOwnProperty.call(elem.attribs, name) &&
-        elem.attribs[name] != null
-    );
+    return elem.attribs?.[name] != null;
 }
 
 /**
