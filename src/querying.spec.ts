d6e84225b85d820fe1ece02a1ceb5a887e8cdae4
yoshitaka-ogata <98735971+yoshitaka-ogata@users.noreply.github.com>
2023-03-03 18:08:07 +0900

    test: add test case for querying.find

---
+++ src/querying.spec.ts
@@ -0,0 +1,23 @@
+import { parseDocument } from "./__fixtures__/fixture";
+import { find } from "./querying";
+import { ElementType } from "domelementtype";
+
+describe("querying", () => {
+    describe("find", () => {
+        it("should accept too many children without stack overflow", () => {
+            let html = "<body>";
+            for (let i = 0; i < 200000; i++) {
+                html += "<div></div>";
+            }
+            html += "</body>";
+            const nodes = [parseDocument(html)];
+            const resultNodes = find(
+                (elem) => elem.type === ElementType.Tag,
+                nodes,
+                true,
+                Infinity
+            );
+            expect(resultNodes).toHaveLength(200001);
+        });
+    });
+});
